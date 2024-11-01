46d51e9468e61bb559fc2cd4d711f22d8da04259
Pooja <studentpooja1234@gmail.com>
2024-11-02 01:41:02 +0530

    To-Do-App Project started

---
+++ 04_React/04_01_REACT_PROJECTS/1-todo-app-version-one/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return <center class='todo-container'>Placeholder</center>
+}
+
+export default App
